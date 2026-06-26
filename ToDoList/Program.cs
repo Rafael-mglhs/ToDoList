85578411905f7d1730957cd37f1cd144f3156195
Rafael <rafaelsouzamagalhaes08@gmail.com>
2026-06-25 23:58:58 -0300

    Feature: Main Menu added

--- ToDoList/Program.cs
+++ ToDoList/Program.cs
@@ -1,9 +1,22 @@
 ﻿namespace ToDoList;
 
+class Task
+{
+    public string Title;
+    public string Description;
+    public bool Complete;
+}
+
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Develop");
+        string bar = "==============================";
+        Console.WriteLine(bar);
+        Console.WriteLine("To Do List\n1 - Add task\n2 - List tasks\n3 - Mark as complete\n4 - Remove task\n5 - Save\n6 - Load\n0 - Exit");
+        Console.Write("Choose an option: ");
+        int option = int.Parse(Console.ReadLine());
+        
+
     }
 }
