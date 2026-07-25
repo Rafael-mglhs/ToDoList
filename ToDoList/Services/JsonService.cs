c41eb76f0081e3525e8e204d929afbff44cb5db0
Rafael <rafaelsouzamagalhaes08@gmail.com>
2026-07-25 12:13:05 -0300

    Feature: functions to Load and save tasks from JSON file added, not implemented yet

--- ToDoList/Services/JsonService.cs
+++ ToDoList/Services/JsonService.cs
@@ -1,6 +1,50 @@
-﻿namespace ToDoList.Services;
+﻿using ToDoList.Models;
+using System.Text.Json;
+using System.IO;
+
+namespace ToDoList.Services;
 
 public class JsonService
 {
+    private static readonly string pathJson = "Data/tasks.json";
+    //Save tasks in JSON file function
+    public static void SaveTasks(List<TaskItem> tasks)
+    {
+        var optionJson = new JsonSerializerOptions { WriteIndented = true };
+        string json = JsonSerializer.Serialize(tasks, optionJson);
+        File.WriteAllText(pathJson, json);
+        return;
+    }
+    
+    //Load tasks from JSON file function
+    public static List<TaskItem> LoadTasks()
+    {
+        if (File.Exists(pathJson))
+        {
+            
+            string readJson = File.ReadAllText(pathJson);
+            if (!string.IsNullOrWhiteSpace(readJson))
+            {
+                List<TaskItem> loadedTasks = JsonSerializer.Deserialize<List<TaskItem>>(readJson);
+                if (loadedTasks == null)
+                {
+                    return [];
+                }
+                else
+                    return (loadedTasks);
+            }
+            else
+            {
+                return [];
+            }
+            
+            
+        }
+        else
+        {
+            return [];
+        }
+        
+    }
     
 }
